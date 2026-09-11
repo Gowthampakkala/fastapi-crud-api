8ea069075bc8c8c9e4d7ec9bd91402cbeaad0758
Pakkala Gowtham <gowthamp200419@gmail.com>
2026-09-11 11:09:22 +0530

    Keep only FastAPI code

--- pandas_practice.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df=pd.read_csv("data.csv", index_col="Name")
-pokemon = input("enter a pokemon name:")
-try:
-    print(df.loc[pokemon])
-except KeyError:
-    print(f"{pokemon} not found")
